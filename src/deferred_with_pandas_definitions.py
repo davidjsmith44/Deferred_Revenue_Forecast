b21010a8c9ecf1bddd523828e00cb1bf216ee91c
Dave Smith <davidjsmith44@yahoo.com>
2020-03-10 15:49:09 -0700

    working on it

--- src/deferred_with_pandas_definitions.py
+++ src/deferred_with_pandas_definitions.py
@@ -8,6 +8,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+'''
+move to below later
+df = pd.read_excel("../data/old/base_billings.xlsx", sheet_name="bill_DC")
+'''
 def load_base_billings(filename, sheetname):
     """
     This function reads the excel file that contains the base billings in document currency
@@ -18,10 +22,28 @@ def load_base_billings(filename, sheetname):
         sheetname: the sheetname that contains the data for the document currency
     OUTPUT:
         df  a dataframe containing the following
+
+
+
+    STEPS:
+        1. Remove currencies that have less than 10 transactions
+        2. Remove values that are zero
+        3. Clear out any Non-Revenue Billings
+        4. Split Apply Combine on the dataframes
+
     """
     df = pd.read_excel("../data/old/base_billings.xlsx", sheet_name="bill_DC")
 
-    # Step 1:A  Remove any currencies that have less than 10 transaction
+    df.rename(index = str, columns = {'Document Currency': 'curr',
+                                 'Enterprise Bu': 'BU',
+                                 'Invoicing Fiscal Year-Period Desc': 'period',
+                                 'Product Configtype ID': 'config',
+                                 'Revenue Recognition Category New': 'rev_req_type',
+                                 'Rule For Bill Date': 'rebill_rule',
+                                 'Completed Sales Doc Currency': 'amount'}, inplace=True)
+
+
+    # Step 1  Remove any currencies that have less than 10 transaction
     vc = df["Document Currency"].value_counts()
     keep_these = vc.values > 10
     keep_curr = vc[keep_these]
@@ -31,6 +53,7 @@ def load_base_billings(filename, sheetname):
     model_dict = {"curr_removed": list(vc[remove_these].index)}
 
     # Need to create some sort of a report with respect to the currencies removed and kept
+    print("---Removing infrequent currencies from billings history---")
     print('Total number of currencies in the base billings file: ', len(vc))
     if sum(remove_these)==0:
         print('No currencies were removed, all contained 10 or more billings')
@@ -38,81 +61,65 @@ def load_base_billings(filename, sheetname):
         for item in a:
             print(a[item], end = " ")
     else:
-        print('{} Currencies were removed'.format(sum(remove_these)))
-        for item in
+        print('\n{} Currencies were removed: '.format(sum(remove_these)))
 
-    for item in a:
-        print(a[item], end = " ")
+        for item in vc[remove_these].index:
+            print(item, ', ', end="")
 
-
-
-
-
+        print("\n\n{} Remaining currencies: ".format(len(a)))
+        for item in a:
+            print(item, ', ', end="")
 
 
-    # Step 1:B Remove any values in the dataframe that are zero
+    # Step 2: Remove any values in the dataframe that are zero
+    print('Removing Zeros from the billings file')
     print("This is the length of the dataframe before removing zeros: ", len(df))
-    df = df[df["Completed Sales Doc Currency"] != 0]
+    df = df[df["amount"] != 0]
     print("This is the length of the dataframe after removing zeros: ", len(df))
 
-    # Step 1:C Clear out any Non-Revenue billings from the file
+
+    # Step 3: Clear out any Non-Revenue billings from the file
     df["Sales Type"].value_counts()
     print("Length of the dataframe before removing non-revenue billings: ", len(df))
     df = df[df["Sales Type"] != "NON-REV"]
     print("Length of the dataframe after removing non-revenue billings:  ", len(df))
 
-    # Step 1:D Split Apply Combine on the dataframes
+
+    # Step 4: Split Apply Combine on the dataframes
     # First split the data into three dataframes
     rec = df[df["Sales Type"] == "RECOGNIZED"]
     svc = df[df["Sales Type"] == "PRO-SVC-INV"]
     dfr = df[df["Sales Type"] == "DEFERRED"]
 
-    # recognized billngs
-    gb_rec = rec.groupby(
-        ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-        as_index=False,
-    ).sum()
+    # RECOGNIZED BILLINGS
+    gb_rec = rec.groupby(["curr", "BU", "period"], as_index=False,).sum()
     gb_rec.drop(labels="Subscription Term", axis=1, inplace=True)
 
-    # service based billings
-    gb_svc = svc.groupby(
-        ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-        as_index=False,
-    ).sum()
+    # SERVICE BASED BILLINGS
+    gb_svc = svc.groupby(["curr", "BU", "Period"],as_index=False,).sum()
     gb_svc.drop(labels="Subscription Term", axis=1, inplace=True)
 
-    # Deferred Billings
-    # Service Based Deferred Billings
-    dfr_b = dfr[dfr["Revenue Recognition Category New"] == "B"]
-    gb_b = dfr_b.groupby(
-        ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-        as_index=False,
-    ).sum()
+    # DEFERRED BILLINGS
+    # Service Based Deferred Billings: Type B
+    dfr_b = dfr[dfr["rev_req_type"] == "B"]
+    gb_b = dfr_b.groupby(["curr", "BU", "Period"],as_index=False,).sum()
     gb_b.drop(labels="Subscription Term", axis=1, inplace=True)
 
     print("length of deferred billings : ", len(dfr))
     print("length of the type B billings: ", len(dfr_b))
 
     # Type A Deferred Billings
-    dfr_a = dfr[dfr["Revenue Recognition Category New"] == "A"]
-    gb_a = dfr_a.groupby(
-        [
-            "Document Currency",
-            "Enterprise Bu",
-            "Invoicing Fiscal Year-Period Desc",
-            "Product Configtype ID",
-        ],
-        as_index=False,
-    ).sum()
+    dfr_a = dfr[dfr["rev_req_type"] == "A"]
+    gb_a = dfr_a.groupby(["Curr", "BU", "Period", "config"],as_index=False,).sum()
     gb_a.drop(labels="Subscription Term", axis=1, inplace=True)
 
     config_list = ["1Y", "2Y", "3Y", "MTHLY"]
-    test1 = gb_a["Product Configtype ID"].isin(config_list)
+    test1 = gb_a["config"].isin(config_list)
 
-    gb_a_1Y = test1[test1["Product Configtype ID"] == "1Y"]
-    gb_a_2Y = test1[test1["Product Configtype ID"] == "2Y"]
-    gb_a_3Y = test1[test1["Product Configtype ID"] == "3Y"]
-    gb_a_1M = test1[test1["Product Configtype ID"] == "MTHLY"]
+    gb_a_1Y = test1[test1["config"] == "1Y"]
+    gb_a_2Y = test1[test1["config"] == "2Y"]
+    gb_a_3Y = test1[test1["config"] == "3Y"]
+    gb_a_1M = test1[test1["config"] == "MTHLY"]
 
     print("this is the lenght of type A 1M billings: ", len(gb_a_1M))
     print("this is the lenght of type A 1Y billings: ", len(gb_a_1Y))
@@ -120,25 +127,17 @@ def load_base_billings(filename, sheetname):
     print("this is the lenght of type A 3Y billings: ", len(gb_a_3Y))
 
     # Type D Billings
-    dfr_d = dfr[dfr["Revenue Recognition Category New"] == "D"]
+    dfr_d = dfr[dfr["rev_req_type"] == "D"]
 
-    gb_d = dfr_d.groupby(
-        [
-            "Document Currency",
-            "Enterprise Bu",
-            "Invoicing Fiscal Year-Period Desc",
-            "Rule For Bill Date",
-        ],
-        as_index=False,
-    ).sum()
+    gb_d = dfr_d.groupby(["curr","BU","Period","rebill_rule"],as_index=False,).sum()
     gb_d.drop(labels="Subscription Term", axis=1, inplace=True)
 
-    gb_d_mthly = gb_d[gb_d["Rule For Bill Date"].isin(["Y1", "Y2", "Y3", "Y5"])]
-    gb_d_qtrly = gb_d[gb_d["Rule For Bill Date"] == "YQ"]
-    gb_d_four_mths = gb_d[gb_d["Rule For Bill Date"] == "YT"]
-    gb_d_semi_ann = gb_d[gb_d["Rule For Bill Date"] == "YH"]
-    gb_d_annual = gb_d[gb_d["Rule For Bill Date"].isin(["YA", "YC"])]
-    gb_d_two_yrs = gb_d[gb_d["Rule For Bill Date"] == "Y"]
+    gb_d_mthly = gb_d[gb_d["rebill_rule"].isin(["Y1", "Y2", "Y3", "Y5"])]
+    gb_d_qtrly = gb_d[gb_d["rebill_rule"] == "YQ"]
+    gb_d_four_mths = gb_d[gb_d["rebill_rule"] == "YT"]
+    gb_d_semi_ann = gb_d[gb_d["rebill_rule"] == "YH"]
+    gb_d_annual = gb_d[gb_d["rebill_rule"].isin(["YA", "YC"])]
+    gb_d_two_yrs = gb_d[gb_d["rebill_rule"] == "Y"]
 
     print("Length of monthly", len(gb_d_mthly))
     print("Length of quarterly", len(gb_d_qtrly))
@@ -151,120 +150,6 @@ def load_base_billings(filename, sheetname):
 """
 END OF COPY FOR FUNCTION
 """
-
-# Step 1: Processing the Base Billings Data
-df = pd.read_excel("../data/old/base_billings.xlsx", sheet_name="bill_DC")
-
-# Step 1:A  Remove any currencies that have less than 10 transaction
-# creates a list of the currencies and the number of transactions for each currency
-vc = df["Document Currency"].value_counts()
-# Create variable that is true if the number of transaction is greater than 10, false otherwise
-keep_these = vc.values > 10
-# filtering only currencies that were greater than 10
-keep_curr = vc[keep_these]
-a = keep_curr.index
-# filtering the dataframe to remove any of teh currencies not in our list
-df = df[df["Document Currency"].isin(a)]
-
-remove_these = vc.values <= 10
-model_dict = {"curr_removed": list(vc[remove_these].index)}
-
-# Step 1:B Remove any values in the dataframe that are zero
-print("This is the length of the dataframe before removing zeros: ", len(df))
-df = df[df["Completed Sales Doc Currency"] != 0]
-print("This is the length of the dataframe after removing zeros: ", len(df))
-
-# Step 1:C Clear out any Non-Revenue billings from the file
-df["Sales Type"].value_counts()
-print("Length of the dataframe before removing non-revenue billings: ", len(df))
-df = df[df["Sales Type"] != "NON-REV"]
-print("Length of the dataframe after removing non-revenue billings:  ", len(df))
-
-# Step 1:D Split Apply Combine on the dataframes
-# First split the data into three dataframes
-rec = df[df["Sales Type"] == "RECOGNIZED"]
-svc = df[df["Sales Type"] == "PRO-SVC-INV"]
-dfr = df[df["Sales Type"] == "DEFERRED"]
-
-# recognized billngs
-gb_rec = rec.groupby(
-    ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-    as_index=False,
-).sum()
-gb_rec.drop(labels="Subscription Term", axis=1, inplace=True)
-
-# service based billings
-gb_svc = svc.groupby(
-    ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-    as_index=False,
-).sum()
-gb_svc.drop(labels="Subscription Term", axis=1, inplace=True)
-
-# Deferred Billings
-# Service Based Deferred Billings
-dfr_b = dfr[dfr["Revenue Recognition Category New"] == "B"]
-gb_b = dfr_b.groupby(
-    ["Document Currency", "Enterprise Bu", "Invoicing Fiscal Year-Period Desc"],
-    as_index=False,
-).sum()
-gb_b.drop(labels="Subscription Term", axis=1, inplace=True)
-
-print("length of deferred billings : ", len(dfr))
-print("length of the type B billings: ", len(dfr_b))
-
-# Type A Deferred Billings
-dfr_a = dfr[dfr["Revenue Recognition Category New"] == "A"]
-gb_a = dfr_a.groupby(
-    [
-        "Document Currency",
-        "Enterprise Bu",
-        "Invoicing Fiscal Year-Period Desc",
-        "Product Configtype ID",
-    ],
-    as_index=False,
-).sum()
-gb_a.drop(labels="Subscription Term", axis=1, inplace=True)
-
-config_list = ["1Y", "2Y", "3Y", "MTHLY"]
-test1 = gb_a["Product Configtype ID"].isin(config_list)
-
-gb_a_1Y = test1[test1["Product Configtype ID"] == "1Y"]
-gb_a_2Y = test1[test1["Product Configtype ID"] == "2Y"]
-gb_a_3Y = test1[test1["Product Configtype ID"] == "3Y"]
-gb_a_1M = test1[test1["Product Configtype ID"] == "MTHLY"]
-
-print("this is the lenght of type A 1M billings: ", len(gb_a_1M))
-print("this is the lenght of type A 1Y billings: ", len(gb_a_1Y))
-print("this is the lenght of type A 2Y billings: ", len(gb_a_2Y))
-print("this is the lenght of type A 3Y billings: ", len(gb_a_3Y))
-
-# Type D Billings
-dfr_d = dfr[dfr["Revenue Recognition Category New"] == "D"]
-
-gb_d = dfr_d.groupby(
-    [
-        "Document Currency",
-        "Enterprise Bu",
-        "Invoicing Fiscal Year-Period Desc",
-        "Rule For Bill Date",
-    ],
-    as_index=False,
-).sum()
-gb_d.drop(labels="Subscription Term", axis=1, inplace=True)
-
-gb_d_mthly = gb_d[gb_d["Rule For Bill Date"].isin(["Y1", "Y2", "Y3", "Y5"])]
-gb_d_qtrly = gb_d[gb_d["Rule For Bill Date"] == "YQ"]
-gb_d_four_mths = gb_d[gb_d["Rule For Bill Date"] == "YT"]
-gb_d_semi_ann = gb_d[gb_d["Rule For Bill Date"] == "YH"]
-gb_d_annual = gb_d[gb_d["Rule For Bill Date"].isin(["YA", "YC"])]
-gb_d_two_yrs = gb_d[gb_d["Rule For Bill Date"] == "Y"]
-
-print("Length of monthly", len(gb_d_mthly))
-print("Length of quarterly", len(gb_d_qtrly))
-print("Length of four months", len(gb_d_four_mths))
-print("Length of semi ann", len(gb_d_semi_ann))
-print("Length of annual", len(gb_d_annual))
-print("Length of two years", len(gb_d_two_yrs))
 
 # Recombining these dataframes into one large dataframe
 # We need to do it this way when we get to a .py file!
