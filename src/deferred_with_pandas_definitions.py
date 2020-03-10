f4f273458df8941ea461dcffdc8c4e5cdce8a361
Dave Smith <davidjsmith44@yahoo.com>
2020-03-10 16:06:22 -0700

    zcc now

--- src/deferred_with_pandas_definitions.py
+++ src/deferred_with_pandas_definitions.py
@@ -8,10 +8,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-'''
+"""
 move to below later
 df = pd.read_excel("../data/old/base_billings.xlsx", sheet_name="bill_DC")
-'''
+"""
+
+
 def load_base_billings(filename, sheetname):
     """
     This function reads the excel file that contains the base billings in document currency
@@ -34,14 +36,19 @@ def load_base_billings(filename, sheetname):
     """
     df = pd.read_excel("../data/old/base_billings.xlsx", sheet_name="bill_DC")
 
-    df.rename(index = str, columns = {'Document Currency': 'curr',
-                                 'Enterprise Bu': 'BU',
-                                 'Invoicing Fiscal Year-Period Desc': 'period',
-                                 'Product Configtype ID': 'config',
-                                 'Revenue Recognition Category New': 'rev_req_type',
-                                 'Rule For Bill Date': 'rebill_rule',
-                                 'Completed Sales Doc Currency': 'amount'}, inplace=True)
-
+    df.rename(
+        index=str,
+        columns={
+            "Document Currency": "curr",
+            "Enterprise Bu": "BU",
+            "Invoicing Fiscal Year-Period Desc": "period",
+            "Product Configtype ID": "config",
+            "Revenue Recognition Category New": "rev_req_type",
+            "Rule For Bill Date": "rebill_rule",
+            "Completed Sales Doc Currency": "amount",
+        },
+        inplace=True,
+    )
 
     # Step 1  Remove any currencies that have less than 10 transaction
     vc = df["Document Currency"].value_counts()
@@ -54,36 +61,33 @@ def load_base_billings(filename, sheetname):
 
     # Need to create some sort of a report with respect to the currencies removed and kept
     print("---Removing infrequent currencies from billings history---")
-    print('Total number of currencies in the base billings file: ', len(vc))
-    if sum(remove_these)==0:
-        print('No currencies were removed, all contained 10 or more billings')
-        print('Currencies in the base billings file')
+    print("Total number of currencies in the base billings file: ", len(vc))
+    if sum(remove_these) == 0:
+        print("No currencies were removed, all contained 10 or more billings")
+        print("Currencies in the base billings file")
         for item in a:
-            print(a[item], end = " ")
+            print(a[item], end=" ")
     else:
-        print('\n{} Currencies were removed: '.format(sum(remove_these)))
+        print("\n{} Currencies were removed: ".format(sum(remove_these)))
 
         for item in vc[remove_these].index:
-            print(item, ', ', end="")
+            print(item, ", ", end="")
 
         print("\n\n{} Remaining currencies: ".format(len(a)))
         for item in a:
-            print(item, ', ', end="")
-
+            print(item, ", ", end="")
 
     # Step 2: Remove any values in the dataframe that are zero
-    print('Removing Zeros from the billings file')
+    print("Removing Zeros from the billings file")
     print("This is the length of the dataframe before removing zeros: ", len(df))
     df = df[df["amount"] != 0]
     print("This is the length of the dataframe after removing zeros: ", len(df))
-
 
     # Step 3: Clear out any Non-Revenue billings from the file
     df["Sales Type"].value_counts()
     print("Length of the dataframe before removing non-revenue billings: ", len(df))
     df = df[df["Sales Type"] != "NON-REV"]
     print("Length of the dataframe after removing non-revenue billings:  ", len(df))
-
 
     # Step 4: Split Apply Combine on the dataframes
     # First split the data into three dataframes
@@ -96,13 +100,13 @@ def load_base_billings(filename, sheetname):
     gb_rec.drop(labels="Subscription Term", axis=1, inplace=True)
 
     # SERVICE BASED BILLINGS
-    gb_svc = svc.groupby(["curr", "BU", "Period"],as_index=False,).sum()
+    gb_svc = svc.groupby(["curr", "BU", "Period"], as_index=False,).sum()
     gb_svc.drop(labels="Subscription Term", axis=1, inplace=True)
 
     # DEFERRED BILLINGS
     # Service Based Deferred Billings: Type B
     dfr_b = dfr[dfr["rev_req_type"] == "B"]
-    gb_b = dfr_b.groupby(["curr", "BU", "Period"],as_index=False,).sum()
+    gb_b = dfr_b.groupby(["curr", "BU", "Period"], as_index=False,).sum()
     gb_b.drop(labels="Subscription Term", axis=1, inplace=True)
 
     print("length of deferred billings : ", len(dfr))
@@ -110,7 +114,7 @@ def load_base_billings(filename, sheetname):
 
     # Type A Deferred Billings
     dfr_a = dfr[dfr["rev_req_type"] == "A"]
-    gb_a = dfr_a.groupby(["Curr", "BU", "Period", "config"],as_index=False,).sum()
+    gb_a = dfr_a.groupby(["Curr", "BU", "Period", "config"], as_index=False,).sum()
     gb_a.drop(labels="Subscription Term", axis=1, inplace=True)
 
     config_list = ["1Y", "2Y", "3Y", "MTHLY"]
@@ -129,7 +133,7 @@ def load_base_billings(filename, sheetname):
     # Type D Billings
     dfr_d = dfr[dfr["rev_req_type"] == "D"]
 
-    gb_d = dfr_d.groupby(["curr","BU","Period","rebill_rule"],as_index=False,).sum()
+    gb_d = dfr_d.groupby(["curr", "BU", "Period", "rebill_rule"], as_index=False,).sum()
     gb_d.drop(labels="Subscription Term", axis=1, inplace=True)
 
     gb_d_mthly = gb_d[gb_d["rebill_rule"].isin(["Y1", "Y2", "Y3", "Y5"])]
@@ -146,42 +150,43 @@ def load_base_billings(filename, sheetname):
     print("Length of annual", len(gb_d_annual))
     print("Length of two years", len(gb_d_two_yrs))
 
+    # Recombining these dataframes into one large dataframe
+    # We need to do it this way when we get to a .py file!
+    list_df = [
+        gb_rec,
+        gb_svc,
+        gb_b,
+        gb_a_1M,
+        gb_a_1Y,
+        gb_a_2Y,
+        gb_a_3Y,
+        gb_d_mthly,
+        gb_d_qtrly,
+        gb_d_semi_ann,
+        gb_d_annual,
+        gb_d_two_yrs,
+    ]
 
-"""
-END OF COPY FOR FUNCTION
-"""
+    list_columns = [
+        "recognized",
+        "service",
+        "deferred_B",
+        "deferred_1M_a",
+        "deferred_1Y_a",
+        "deferred_2Y_a",
+        "deferred_3Y_a",
+        "deferred_1M_d",
+        "deferred_3M_d",
+        "deferred_6M_d",
+        "deferred_1Y_d",
+        "deferred_2Y_d",
+    ]
 
-# Recombining these dataframes into one large dataframe
-# We need to do it this way when we get to a .py file!
-list_df = [
-    gb_rec,
-    gb_svc,
-    gb_b,
-    gb_a_1M,
-    gb_a_1Y,
-    gb_a_2Y,
-    gb_a_3Y,
-    gb_d_mthly,
-    gb_d_qtrly,
-    gb_d_semi_ann,
-    gb_d_annual,
-    gb_d_two_yrs,
-]
+    df = merge_all_dataframes(list_df, list_columns)
 
-list_columns = [
-    "recognized",
-    "service",
-    "deferred_B",
-    "deferred_1M_a",
-    "deferred_1Y_a",
-    "deferred_2Y_a",
-    "deferred_3Y_a",
-    "deferred_1M_d",
-    "deferred_3M_d",
-    "deferred_6M_d",
-    "deferred_1Y_d",
-    "deferred_2Y_d",
-]
+    df = clean_df_columns(df)
+
+    return df
 
 
 def merge_new_dataframe(old_df, new_df, new_column):
@@ -189,29 +194,19 @@ def merge_new_dataframe(old_df, new_df, new_column):
         old_df,
         new_df,
         how="outer",
-        left_on=[
-            "Document Currency",
-            "Enterprise Bu",
-            "Invoicing Fiscal Year-Period Desc",
-        ],
-        right_on=[
-            "Document Currency",
-            "Enterprise Bu",
-            "Invoicing Fiscal Year-Period Desc",
-        ],
+        left_on=["curr", "BU", "Period"],
+        right_on=["curr", "BU", "Period"],
     )
-    df_merged.rename(
-        index=str, columns={"Completed Sales Doc Currency": new_column}, inplace=True
-    )
+    df_merged.rename(index=str, columns={"amount": new_column}, inplace=True)
 
     # need to drop the product configtype id for merges where the new_df is of type A
-    config_str = "Product Configtype ID"
-    rule_str = "Rule For Bill Date"
+    config_str = "config"
+    rule_str = "rebill_rule"
     if config_str in df_merged.columns:
-        df_merged.drop(columns=["Product Configtype ID"], inplace=True)
+        df_merged.drop(columns=["config"], inplace=True)
 
     if rule_str in df_merged.columns:
-        df_merged.drop(columns=["Rule For Bill Date"], inplace=True)
+        df_merged.drop(columns=["rebill_rule"], inplace=True)
 
     return df_merged
 
@@ -225,17 +220,12 @@ def merge_all_dataframes(list_df, list_columns):
         if i == 0:
             df_merged = list_df[0]
             df_merged.rename(
-                index=str,
-                columns={"Completed Sales Doc Currency": list_columns[i]},
-                inplace=True,
+                index=str, columns={"amount": list_columns[i]}, inplace=True
             )
         else:
             df_merged = merge_new_dataframe(df_merged, df, list_columns[i])
 
     return df_merged
-
-
-df = merge_all_dataframes(list_df, list_columns)
 
 
 def clean_df_columns(df):
@@ -266,21 +256,28 @@ def clean_df_columns(df):
         inplace=True,
     )
 
-    # cleaning up the longer column names
-    df.rename(
-        index=str,
-        columns={
-            "Document Currency": "curr",
-            "Enterprise Bu": "BU",
-            "Invoicing Fiscal Year-Period Desc": "period",
-        },
-        inplace=True,
-    )
-
     return df
 
 
 df = clean_df_columns(df)
 
-""" Stopped after the base billings have been loaded up"""
+""" Now working on the ZCC billings """
+df_cal = pd.read_excel("../data/old/ADOBE_FINANCIAL_CALENDAR.xlsx", "ADBE_cal")
+
+df_ZCC = pd.read_excel("../data/old/type_D_ZCC_billings.xlsx", sheet_name="DC")
+
+df_ZCC.rename(
+    index=str,
+    columns={
+        "Document Currency": "curr",
+        "Enterprise BU Description": "BU",
+        "Rule for Bill Date Code": "rebill_rule",
+        "Week of FICA Posting Date (YYYYMMDD) (copy)": "fiscal_week",
+        "DF Additions - Doc Curr": "amount",
+    },
+    inplace=True,
+)
+
+
+ZCC_curr = df_ZCC["curr"].unique()
 
